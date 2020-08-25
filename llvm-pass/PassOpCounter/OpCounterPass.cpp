62dded911349ad498d66e4d496d8654b9d7ef534
WANG Bo <wbprosci@outlook.com>
2020-08-26 01:21:17 +0800

    add a pass that count op in each function.

---
+++ llvm-pass/PassOpCounter/OpCounterPass.cpp
@@ -0,0 +1,55 @@
+#include "llvm/IR/Function.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Passes/PassBuilder.h"
+
+using namespace llvm;
+using CounterMap = llvm::StringMap<unsigned>;
+
+namespace {
+
+CounterMap generateOpCounterMap(Function &F) {
+    CounterMap map;
+    for(llvm::BasicBlock &BB : F) {
+        for(llvm::Instruction &Inst : BB) {
+            StringRef opName = Inst.getOpcodeName();
+            if (map.find(opName) == map.end()) {
+                map[opName] = 1;
+            } else {
+                map[opName] += 1;
+            }
+        }
+    }
+    return map;
+}
+
+void printOpcodeCounterResult(const CounterMap &result) {
+    for (auto & s : result) {
+        errs() << s.getKey() << ": " << s.getValue() << "\n";
+    }
+}
+
+struct OpcodeCounter : public llvm::FunctionPass {
+    static char ID;
+    OpcodeCounter() : llvm::FunctionPass(ID) {}
+    bool runOnFunction(llvm::Function &F) override {
+        errs() << "=============" << F.getName() << "=============" << '\n';
+        CounterMap result = generateOpCounterMap(F);
+        printOpcodeCounterResult(result);
+        return false;
+    }
+};
+
+}
+
+char OpcodeCounter::ID = 0;
+static RegisterPass<OpcodeCounter> X(
+    "opcounter", 
+    "Opcode Counter Pass",
+     false, 
+     false);
+
+
+
+
--- llvm-pass/runner.sh
+++ llvm-pass/runner.sh
@@ -1,3 +1,7 @@
-clang `llvm-config --cxxflags` -fno-rtti -fPIC -shared ./PassHello/HelloPass.cpp -o ./PassHello/LLVMHelloPass.so
+# clang `llvm-config --cxxflags` -fno-rtti -fPIC -shared ./PassHello/HelloPass.cpp -o ./PassHello/LLVMHelloPass.so
+# clang -emit-llvm -c ./CommonTests/test0/main.cpp -o ./CommonTests/test0/main.bc
+# opt -load ./PassHello/LLVMHelloPass.so -hello ./CommonTests/test0/main.bc -o /dev/null
+
+clang `llvm-config --cxxflags` -fno-rtti -fPIC -shared ./PassOpCounter/OpCounterPass.cpp -o ./PassOpCounter/LLVMOpCounterPass.so
 clang -emit-llvm -c ./CommonTests/test0/main.cpp -o ./CommonTests/test0/main.bc
-opt -load ./PassHello/LLVMHelloPass.so -hello ./CommonTests/test0/main.bc -o /dev/null
+opt -load ./PassOpCounter/LLVMOpCounterPass.so -opcounter ./CommonTests/test0/main.bc -o /dev/null
